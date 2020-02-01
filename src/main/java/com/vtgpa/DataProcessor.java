038a3e4c20a46e75dbee44a738807f8d034f69e5
rynmrcs <rynmrcs@vt.edu>
2020-02-01 11:00:20 -0500

    Nice

--- src/main/java/com/vtgpa/DataProcessor.java
+++ src/main/java/com/vtgpa/DataProcessor.java
@@ -84,7 +84,7 @@ public class DataProcessor {
     
     @RequestMapping("test/{subject}/{courseNum}/{instructor}")
     public String getGPAOnly(@PathVariable("subject")String subject, @PathVariable("courseNum")String courseNum, @PathVariable("instructor")String instructor) {
-    	if (getGPA(subject, courseNum, instructor).getInstructorGPA().equals("0.0")) {
+    	if (getGPA(subject, courseNum, instructor).getInstructorGPA().equals("0")) {
     		return getGPA(subject, courseNum, instructor).getCourseGPA()+"*";
     	}
     	return getGPA(subject, courseNum, instructor).getInstructorGPA();
